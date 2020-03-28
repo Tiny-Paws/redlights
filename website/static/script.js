73ee9b627d42c9c7cc6f2636da370de2a9599206
shizzuru <bastien.mill@yahoo.fr>
2020-03-28 21:49:16 +0100

    website boilerplate

---
+++ website/static/script.js
@@ -0,0 +1,20 @@
+redlight = new Vue({
+    el: "#red",
+    data: {
+        color: "red"
+    }
+})
+
+orangelight = new Vue({
+    el: "#orange",
+    data: {
+        color: "orange"
+    }
+})
+
+greenlight = new Vue({
+    el: "#green",
+    data: {
+        color: "green"
+    }
+})
